286d0f012cc5b7e845ec30740d9e5021a80b0be4
chrialge <chrialge99@gmail.com>
2024-04-03 16:24:39 +0200

    add flag for ever country

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -9,8 +9,46 @@ export default {
     },
     data() {
         return {
-            
+            style: "/flat/32.png",
+            languageFlag: this.language,
+            urlFlag: 'https://flagsapi.com',
+            flag: ''
         }
+    },
+    methods:{
+
+        flagGenerate(language){
+            console.log(this.languageFlag)
+            this.languageFlag = (language).toUpperCase()
+            if(this.languageFlag == 'EN'){
+                this.languageFlag = 'GB'
+                this.flag = this.urlFlag + '/' + this.languageFlag + this.style
+                return this.flag
+            }else if(this,this.languageFlag == 'JA'){
+                this.languageFlag = 'JP'
+                this.flag = this.urlFlag + '/' + this.languageFlag + this.style
+                return this.flag
+            }else if(this,this.languageFlag == 'ZH'){
+                this.languageFlag = 'CN'
+                this.flag = this.urlFlag + '/' + this.languageFlag + this.style
+                return this.flag
+            }else if(this,this.languageFlag == 'KO'){
+                this.languageFlag = 'KR'
+                this.flag = this.urlFlag + '/' + this.languageFlag + this.style
+                return this.flag
+            }else if(this,this.languageFlag == 'FA'){
+                this.languageFlag = 'CM'
+                this.flag = this.urlFlag + '/' + this.languageFlag + this.style
+                return this.flag
+            }
+            else{
+
+                this.flag = this.urlFlag + '/' + this.languageFlag + this.style
+                return this.flag
+            }
+        }
+    },
+    mounted(){
     }
 }
 </script>
@@ -18,7 +56,7 @@ export default {
     <ul>
         <li>{{ title}}</li>
         <li>{{ titleOriginal }}</li>
-        <li>{{ language }}</li>
+        <li v-if="flagGenerate(language)"><img :src="flag" alt="">{{ language }}</li>
         <li>{{ vote }}</li>
     </ul>
 </template>
