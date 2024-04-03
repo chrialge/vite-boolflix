00e4af94a6706787c2184bdb548911ff9ecf64b7
chrialge <chrialge99@gmail.com>
2024-04-03 11:21:33 +0200

    add input, button and trial functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
-<script >
-
+<script>
+export default{
+  name: app,
+  data(){
+    return{
+      searchFilm: 'cf',
+    }
+  },
+  methods:{
+    search(){
+      console.log(this.searchFilm)
+    }
+  },
+  mounted(){
+    console.log(this.searchFilm)
+    console.log('ciao')
+  }
+}
 </script>
 
 <template>
-
+  <header>
+    <input type="search" name="search_film" id="" v-model="searchFilm" @keyup="search">
+    <button>click me!</button>
+  </header>
 </template>
 
 <style>
